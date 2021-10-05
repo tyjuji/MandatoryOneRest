f3c2f505ac27cc3a1520ae92f3c0e197987d9844
tyjuji <tyjuji@gmx.com>
2021-10-05 20:20:15 +0200

    Add project files.

---
+++ MandatoryOneRest/Controllers/BooksController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MandatoryOneLibrary;
+using MandatoryOneRest.Managers;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MandatoryOneRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        BooksManager _manager = new BooksManager();
+
+        // GET: api/<BooksController>
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/<BooksController>/5
+        [HttpGet("{ISBN13}")]
+        public Book Get(string ISBN13)
+        {
+            return _manager.GetByISBN(ISBN13);
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public void Post([FromBody] Book book)
+        {
+            _manager.PostBook(book);
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{ISBN13}")]
+        public void Put(string ISBN13, [FromBody] Book book)
+        {
+        }
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{ISBN13}")]
+        public void Delete(string ISBN13)
+        {
+        }
+    }
+}
---
+++ MandatoryOneRest/Managers/BooksManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MandatoryOneLibrary;
+
+namespace MandatoryOneRest.Managers
+{
+    public class BooksManager
+    {
+        private List<Book> _books = new List<Book>{ 
+            new Book {Title = "Computer Networking, Global Edition", 
+                    Author = "James Kurose, Keith Ross", 
+                    PageNumber = 800, ISBN13 = "9781292405469"},
+            new Book {Title = "Extreme Programming Explained",
+                    Author = "Kent Beck, Cynthia Andres",
+                    PageNumber = 224, ISBN13 = "9780321278654"},
+            new Book {Title = "The Agile Samurai",
+                    Author = "Jonathan Rasmusson",
+                    PageNumber = 280, ISBN13 = "9781934356586"}
+            };
+
+
+        public IEnumerable<Book> GetAll()
+        {
+            return _books;
+        }
+
+        public Book GetByISBN(string isbn)
+        {
+            return _books.Find(x => x.ISBN13 == isbn);
+        }
+
+        public Book PostBook(Book book)
+        {
+            if(!_books.Contains(book))
+            {
+                _books.Add(book);
+            }
+
+            return book;
+        }
+
+        public Book PutBook(Book book)
+        {
+           return book; // Not finished
+        }
+
+        public void DeleteBook(string isbn)
+        {
+            _books.RemoveAll(x => x.ISBN13 == isbn);
+        }
+    }
+}
